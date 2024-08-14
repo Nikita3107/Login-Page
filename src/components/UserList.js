3de09d8d14b9d2a96b11bf881aee07985253c65e
Nikita <patidarnikita826@gmail.com>
2024-08-14 16:59:25 +0530

    second commit

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const UserList = () => {
+    const[users,setUsers]=useState([]);
+    const[loading,setLoading]=useState('true');
+    const[error,setError]=useState('null');
+
+    useEffect=()=>{
+        fetch('https://mmfinfotech.co/machine_test/api/userList?page=1')
+    }
+
   return (
     <div>UserList</div>
   )
