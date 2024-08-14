17e7882f42d8ac64a6002eabe0dd2a89d6f81d0d
Nikita <patidarnikita826@gmail.com>
2024-08-14 17:15:54 +0530

    userlist error

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -5,9 +5,21 @@ const UserList = () => {
     const[loading,setLoading]=useState('true');
     const[error,setError]=useState('null');
 
-    useEffect=()=>{
+    useEffect=(()=>{
         fetch('https://mmfinfotech.co/machine_test/api/userList?page=1')
-    }
+        .then((response)=>response.json())
+        .then((data)=>{
+            setUsers(data);
+            setLoading(false)
+        })
+        .catch((error)=>{
+            setError('failed to fetch user')
+            setLoading(false)
+        })
+    },[])
+
+    if(loading)return<p>Loading...</p>
+    if(error)return<p style={{color:'red'}}>{error}</p>
 
   return (
     <div>UserList</div>
