f85f17ce967ab6669c812ea453772878eaaaab99
Nikita <patidarnikita826@gmail.com>
2024-08-14 17:56:37 +0530

    hadle login commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isLoggedIn,setIsLoggedIn]=useState(false)
+  const[isAdmin,setIsAdmin]=useState(false)
+
+  const handleLoginSuccess=()=>{
+    setIsLoggedIn(true)
+    setIsAdmin(true)
+  }
+  const handleSignUpSuccess=()=>{
+    setIsLoggedIn(true)
+  }
+  const handleLogin=()=>{
+    
+  }
   return (
     <div className="App">
      
