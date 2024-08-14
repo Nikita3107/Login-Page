07d013bef2f1a9633aebfc8f093aa74abf2f44f4
Nikita <patidarnikita826@gmail.com>
2024-08-14 16:38:06 +0530

    final commit

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react'
+import DashboardCount from './DashboardCount';
+import UserList from './UserList';
 
 const AdminDashboard = () => {
     const[selectTab , setSelectTab]=useState('dashboard');
 
   return (
-    <div>AdminDashboard</div>
+    <>
+    <h2>AdminDashboard</h2>
+    <div>
+        <button onClick={()=>setSelectTab('dashboard')}>Dashboard Count</button>
+        <button onClick={()=>setSelectTab('userlist')}>User List</button>
+        </div>
+        {selectTab==='dashboard'&&<DashboardCount/>}
+        {selectTab==='userlist'&&<UserList/>}
+
+    </>
   )
 }
 
