6c4c2ebb597ca44008cb8cc2810c0f19a9661137
Nikita <patidarnikita826@gmail.com>
2024-08-14 16:39:56 +0530

    formate doc. commit

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -1,22 +1,23 @@
-import React, { useState } from 'react'
-import DashboardCount from './DashboardCount';
-import UserList from './UserList';
+import React, { useState } from "react";
+import DashboardCount from "./DashboardCount";
+import UserList from "./UserList";
 
 const AdminDashboard = () => {
-    const[selectTab , setSelectTab]=useState('dashboard');
+  const [selectTab, setSelectTab] = useState("dashboard");
 
   return (
     <>
-    <h2>AdminDashboard</h2>
-    <div>
-        <button onClick={()=>setSelectTab('dashboard')}>Dashboard Count</button>
-        <button onClick={()=>setSelectTab('userlist')}>User List</button>
-        </div>
-        {selectTab==='dashboard'&&<DashboardCount/>}
-        {selectTab==='userlist'&&<UserList/>}
-
+      <h2>AdminDashboard</h2>
+      <div>
+        <button onClick={() => setSelectTab("dashboard")}>
+          Dashboard Count
+        </button>
+        <button onClick={() => setSelectTab("userlist")}>User List</button>
+      </div>
+      {selectTab === "dashboard" && <DashboardCount />}
+      {selectTab === "userlist" && <UserList />}
     </>
-  )
-}
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
