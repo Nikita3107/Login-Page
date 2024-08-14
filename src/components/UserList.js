84977cfa875413001d851d943074a88bfb015c00
Nikita <patidarnikita826@gmail.com>
2024-08-14 17:41:34 +0530

    formate doc. commit

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,38 +1,40 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const UserList = () => {
-    const[users,setUsers]=useState([]);
-    const[loading,setLoading]=useState('true');
-    const[error,setError]=useState('null');
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState("true");
+  const [error, setError] = useState("null");
 
-    useEffect=(()=>{
-        fetch('https://mmfinfotech.co/machine_test/api/userList?page=1')
-        .then((response)=>response.json())
-        .then((data)=>{
-            setUsers(data);
-            setLoading(false)
+  useEffect =
+    (() => {
+      fetch("https://mmfinfotech.co/machine_test/api/userList?page=1")
+        .then((response) => response.json())
+        .then((data) => {
+          setUsers(data);
+          setLoading(false);
         })
-        .catch((error)=>{
-            setError('failed to fetch user')
-            setLoading(false)
-        })
-    },[])
+        .catch((error) => {
+          setError("failed to fetch user");
+          setLoading(false);
+        });
+    },
+    []);
 
-    if(loading)return<p>Loading...</p>
-    if(error)return<p style={{color:'red'}}>{error}</p>
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p style={{ color: "red" }}>{error}</p>;
 
   return (
     <>
-    <h2>UserList</h2>
-    <ul>
-        {users.map((user)=>(
-            <li key={user.id}>{user.name}-{user.email}</li>
-        ))
-
-        }
-    </ul>
+      <h2>UserList</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name}-{user.email}
+          </li>
+        ))}
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default UserList
+export default UserList;
