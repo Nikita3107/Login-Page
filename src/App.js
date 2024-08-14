f023e477c2f3d73addbb6ddc6b8499521a60b673
Nikita <patidarnikita826@gmail.com>
2024-08-14 18:51:05 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import AdminDashboard from "./components/AdminDashboard";
+import Login from "./components/Login";
+import Home from "./components/Home";
 
 function App() {
-  const [isLoggedIn,setIsLoggedIn]=useState(false)
-  const[isAdmin,setIsAdmin]=useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
 
-  const handleLoginSuccess=()=>{
-    setIsLoggedIn(true)
-    setIsAdmin(true)
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true);
+    setIsAdmin(true);
+  };
+  const handleSignUpSuccess  = () => {
+    setIsLoggedIn(true);
+  };
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setIsAdmin(false);
+  };
+
+  if (!isLoggedIn) {
+    return <Login onLoginSuccess={handleLoginSuccess} />;
   }
-  const handleSignUpSuccess=()=>{
-    setIsLoggedIn(true)
+  if (isLoggedIn && isAdmin) {
+    return <AdminDashboard />;
   }
-  const handleLogin=()=>{
-    
-  }
-  return (
-    <div className="App">
-     
-    </div>
-  );
+  return <Home  onLogout={handleLogout}/>;
 }
 
 export default App;
